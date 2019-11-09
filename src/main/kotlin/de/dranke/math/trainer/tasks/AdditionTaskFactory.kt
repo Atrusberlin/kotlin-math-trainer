62985aed7fef39fea28cb6e4b236890a10f5fc90
Daniel Ranke <daniel.ranke@hotmail.de>
2019-11-09 21:47:28 +0100

    Simple task generation implemented
    Addition task with given values
    
    #1

---
+++ src/main/kotlin/de/dranke/math/trainer/tasks/AdditionTaskFactory.kt
@@ -0,0 +1,17 @@
+package de.dranke.math.trainer.tasks
+
+class AdditionTaskFactory {
+    val operation: Char = '+'
+
+    fun createTask(): Task {
+        return Task(1, 2, operation);
+    }
+
+    class Task(val addend1: Int, val addend2: Int, val operation: Char) {
+
+        init {
+            println("New Task: $addend1 $operation $addend2 = ?")
+        }
+
+    }
+}
---
+++ src/test/kotlin/de/dranke/math/trainer/tasks/AdditionTaskFactoryTest.kt
@@ -0,0 +1,31 @@
+package de.dranke.math.trainer.tasks
+
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class AdditionTaskFactoryTest {
+
+    private lateinit var task: AdditionTaskFactory.Task
+
+    @BeforeTest
+    fun setup() {
+        task = AdditionTaskFactory().createTask()
+    }
+
+    @Test
+    fun the_task_operation_is_plus() {
+
+        assertEquals('+', task.operation, "Operation has to be '+'")
+    }
+
+    @Test
+    fun the_first_addent_is_1() {
+        assertEquals(1, task.addend1, "First addent has to be 1")
+    }
+
+    @Test
+    fun the_second_addent_is_2() {
+        assertEquals(2, task.addend2, "Second addent has to be 2")
+    }
+}
